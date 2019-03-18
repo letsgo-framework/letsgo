c2cadf14373fca987ca1bb9267ab0f1697e47b66
Sabyasachi Patra <asabya@gmail.com>
2019-03-18 09:14:13 +0530

    godotenv added

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	r := gin.Default()
 
 	v1 := r.Group("/api/v1")
@@ -14,6 +21,6 @@ func main() {
 		})
 
 	}
-	r.Run()
+	r.Run(os.Getenv("PORT"))
 
 }
