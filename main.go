2935755c2f9bcc625000294e439bbd0faf9bfd21
Sabyasachi Patra <asabya@gmail.com>
2019-03-19 09:54:16 +0530

    mongo configured

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"os"
+	"time"
 )
 
+var db *mongo.Database
+
 func main() {
+	// Load enc
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	// Connect DB
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbHost := os.Getenv("DATABASE_HOST")
+		dbPort := os.Getenv("DATABASE_PORT")
+		dbURL = fmt.Sprintf("mongodb://%s:%s", dbHost, dbPort)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(dbURL))
+	db = client.Database(os.Getenv("DATABASE"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
 	r := gin.Default()
 
+	// Grouped api
 	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/", func(c *gin.Context) {
@@ -21,6 +47,6 @@ func main() {
 		})
 
 	}
-	r.Run(os.Getenv("PORT"))
 
+	r.Run(os.Getenv("PORT"))
 }
