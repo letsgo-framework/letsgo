ecfadb1fe1896321a155aa7dd17e731241db1d86
Sabyasachi Patra <asabya@gmail.com>
2019-03-26 08:52:19 +0530

    added auth tests

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,10 +1,13 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gitlab.com/letsgo/routes"
+	"gitlab.com/letsgo/types"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -81,6 +84,155 @@ func (s *MySuite) TestHelloWorld(c *C) {
 	defer resp.Body.Close()
 	c.Assert(resp.StatusCode, Equals, 200)
 }
+
+
+func (s *MySuite) TestCredentials(c *C) {
+	requestURL := "http://127.0.0.1:8084/api/v1/credentials/"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	c.Assert(resp.StatusCode, Equals, 200)
+}
+
+func (s *MySuite) TestTokenSuccess(c *C) {
+	requestURL := "http://127.0.0.1:8084/api/v1/credentials/"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	var credResponse types.CredentialResponse
+	json.Unmarshal(responseData, &credResponse)
+
+	requestURL = "http://127.0.0.1:8084/api/v1/token?grant_type=client_credentials&client_id="+credResponse.CLIENT_ID+"&client_secret="+credResponse.CLIENT_SECRET+"&scope=read"
+
+	req, err = http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err = client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	c.Assert(resp.StatusCode, Equals, 200)
+}
+
+func (s *MySuite) TestTokenFail(c *C) {
+	requestURL := "http://127.0.0.1:8084/api/v1/token"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	c.Assert(resp.StatusCode, Equals, 401)
+}
+
+func (s *MySuite) TestAccessTokenSuccess(c *C) {
+	requestURL := "http://127.0.0.1:8084/api/v1/credentials/"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	var credResponse types.CredentialResponse
+	json.Unmarshal(responseData, &credResponse)
+
+	requestURL = "http://127.0.0.1:8084/api/v1/token?grant_type=client_credentials&client_id="+credResponse.CLIENT_ID+"&client_secret="+credResponse.CLIENT_SECRET+"&scope=read"
+
+	req, err = http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err = client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	var tokenResponse types.TokenResponse
+	json.Unmarshal(respData, &tokenResponse)
+
+	requestURL = "http://127.0.0.1:8084/api/v1/auth?access_token="+tokenResponse.Access_token
+
+	req, err = http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err = client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+
+
+	c.Assert(resp.StatusCode, Equals, 200)
+}
+
+
+func (s *MySuite) TestAccessTokenFail(c *C) {
+	requestURL := "http://127.0.0.1:8084/api/v1/credentials/"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err := client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+	responseData, err := ioutil.ReadAll(resp.Body)
+	var credResponse types.CredentialResponse
+	json.Unmarshal(responseData, &credResponse)
+
+	requestURL = "http://127.0.0.1:8084/api/v1/token?grant_type=client_credentials&client_id="+credResponse.CLIENT_ID+"&client_secret="+credResponse.CLIENT_SECRET+"&scope=read"
+
+	req, err = http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err = client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+
+	respData, err := ioutil.ReadAll(resp.Body)
+	var tokenResponse types.TokenResponse
+	json.Unmarshal(respData, &tokenResponse)
+
+	requestURL = "http://127.0.0.1:8084/api/v1/auth?access_token=mywrongaccesstoken"
+
+	req, err = http.NewRequest("GET", requestURL, nil)
+	req.Header.Set(`X_API_KEY`,os.Getenv("X_API_KEY"))
+	resp, err = client.Do(req)
+	if err != nil {
+		c.Error(err)
+		c.Fail()
+	}
+	defer resp.Body.Close()
+
+
+	c.Assert(resp.StatusCode, Equals, 401)
+}
+
 
 func Test(t *testing.T) {
 	TestingT(t)
