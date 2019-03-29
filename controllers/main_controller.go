63cde7ba7b37ae8006152de5ba80964a90583fa3
Sabyasachi Patra <asabya@gmail.com>
2019-03-29 09:27:25 +0530

    pretty welcome

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -6,7 +6,18 @@ import (
 
 
 // The content below is only a placeholder and can be replaced.
+type Welcome struct {
+	Greet string `json:"greet"`
+	Doc string `json:"link_to_doc"`
+	Github string `json:"github"`
+	Examples string `json:"examples"`
+}
 func Home(c *gin.Context) {
-	c.String(200, `Welcome to letsGo`)
+	var welcome Welcome
+	welcome.Greet = `Welcome to letsGo`
+	welcome.Doc = `Link to readme`
+	welcome.Github = `Link to github`
+	welcome.Examples = `Link To examples`
+	c.JSON(200, welcome)
 	c.Done()
 }
