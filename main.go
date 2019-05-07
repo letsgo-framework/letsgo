f2b11f350d8a9af32217a293b83902f4e200ff6d
Sabyasachi Patra <asabya@gmail.com>
2019-05-07 17:11:10 +0530

    log support added

--- main.go
+++ main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	"github.com/letsGo/database"
-	"github.com/letsGo/routes"
+	"github.com/letsgo-framework/letsgo/database"
+	"github.com/letsgo-framework/letsgo/routes"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"log"
 	"os"
 )
 
 func main() {
+
+	// Configure Logging
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "./log/letsgo.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,   //days
+		Compress:   true, // disabled by default
+	})
+
+
 	// Load env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	} else {
+		log.Println("env loaded")
 	}
 
 	database.Connect()
